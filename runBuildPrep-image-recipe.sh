7bea434828b0eb75631ac557188d4084154ee7c8
Jason Sollom <jason.sollom@hpe.com>
2021-02-15 13:50:18 -0600

    CASMCMS-6704: Fix the UAN play
    Check SLS for the CAN element, using the correct index which is 2 not 0.
    Previously, this play was not getting the CAN information.
    
    Remove the can-up and can-down calls as they are not needed when using
    ifroute to set the CAN route.
    
    Create a bond0 ethernet device.
    
    For ifroute-vlan007 remove the 'table 3' as it is not used in Shasta 1.4
    for routing. Remove the netmask.

--- runBuildPrep-image-recipe.sh
+++ runBuildPrep-image-recipe.sh
@@ -6,11 +6,7 @@ source ./vars.sh
 sed -i s/CRAY.VERSION.HERE/${IMG_VER}/g images/kiwi-ng/cray-sles15sp1-uan-cos/config-template.xml.j2
 
 # Set the cray-ims-load-artifacts image version
-# The URL to the manifest.txt file must be updated to point to the stable manifest when cutting a release branch.
-wget https://arti.dev.cray.com/artifactory/csm-misc-master-local/manifest/manifest.txt
-ims_load_artifacts_image_tag=$(cat manifest.txt | grep cray-ims-load-artifacts | sed s/.*://g | tr -d '[:space:]')
-sed -i s/@ims_load_artifacts_image_tag@/${ims_load_artifacts_image_tag}/g Dockerfile.image-recipe
-rm manifest.txt
+./update_tags.sh
 
 # Set the product version in the Dockerfile.image-recipe file
 sed -i s/@product_version@/${VERSION}/g Dockerfile.image-recipe

e23b0e382643a654993b96eedab8235a30f2397d
Alan Mutschelknaus <alanm@cray.com>
2021-07-29 12:53:13 -0500

    Add a Jenkinsfile.github and remove DST pipelines

--- vars.sh
+++ vars.sh
@@ -9,10 +9,3 @@ export RELEASE_VERSION="master"
 export SLINGSHOT_RELEASE_VERSION="master"
 export CSM_RELEASE_VERSION="master"
 export COS_RELEASE_VERSION="master"
-
-# Artifact Bloblet Locations for UAN and its dependencies
-export BLOBLET_UAN="https://artifactory.algol60.net/artifactory/uan-rpms/hpe/stable"
-export BLOBLET_SLINGSHOT="http://dst.us.cray.com/dstrepo/bloblets/slingshot-host-software/${RELEASE_PREFIX}/${SLINGSHOT_RELEASE_VERSION}"
-export BLOBLET_CSM="http://dst.us.cray.com/dstrepo/bloblets/csm/${RELEASE_PREFIX}/${CSM_RELEASE_VERSION}"
-export BLOBLET_COS="http://dst.us.cray.com/dstrepo/bloblets/cos/${RELEASE_PREFIX}/${COS_RELEASE_VERSION}"
-export BLOBLET_OS="http://dst.us.cray.com/dstrepo/bloblets/os/dev/mirrors"

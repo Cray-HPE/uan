8be7e16ec9975094b7a88b1ec5339973b02b7c3b
Jeff Keopp <keopp@cray.com>
2021-10-28 16:56:42 -0500

    CASMUSER-2624: Update BLOBLET_SLINGSHOT and BLOBLET_COS locations

--- vars.sh
+++ vars.sh
@@ -12,7 +12,7 @@ export COS_RELEASE_VERSION="master"
 
 # Artifact Bloblet Locations for UAN and its dependencies
 export BLOBLET_UAN="https://artifactory.algol60.net/artifactory/uan-rpms/hpe/stable"
-export BLOBLET_SLINGSHOT="http://dst.us.cray.com/dstrepo/bloblets/slingshot-host-software/${RELEASE_PREFIX}/${SLINGSHOT_RELEASE_VERSION}"
+export BLOBLET_SLINGSHOT="http://dst.us.cray.com/dstrepo/bloblets/slingshot-host-software/release/cos-2.1/"
 export BLOBLET_CSM="http://dst.us.cray.com/dstrepo/bloblets/csm/${RELEASE_PREFIX}/${CSM_RELEASE_VERSION}"
-export BLOBLET_COS="http://dst.us.cray.com/dstrepo/bloblets/cos/${RELEASE_PREFIX}/${COS_RELEASE_VERSION}"
+export BLOBLET_COS="http://dst.us.cray.com/dstrepo/bloblets/cos/release/cos-2.1"
 export BLOBLET_OS="http://dst.us.cray.com/dstrepo/bloblets/os/dev/mirrors"
